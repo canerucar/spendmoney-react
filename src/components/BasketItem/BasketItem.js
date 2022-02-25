8dc0f7b3990cb48b8d818866885fb737ad712bb8
Caner Uçar <canerucar0@gmail.com>
2022-02-25 22:07:35 +0300

    feature : Add BasketItem

---
+++ src/components/BasketItem/BasketItem.js
@@ -0,0 +1,13 @@
+import "./index.scss";
+
+function BasketItem({ item, product }) {
+  return (
+    <>
+      <li className="basket-item">
+        {product.title} <span>x {item.amount}</span>
+      </li>
+    </>
+  );
+}
+
+export default BasketItem;
